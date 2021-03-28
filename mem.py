912e52044251ef3f70936139d8367c308fbcfcc2
Bharat-Sharma-afk <79968768+Bharat-Sharma-afk@users.noreply.github.com>
2021-03-28 15:21:36 +0530

    Template
    Check if the given template is correct and write the Input Arguments and the return Argument for the functions.

--- mem.py
+++ mem.py
@@ -1,37 +1,66 @@
 #memory access
 
 #create memory class
-#create dictionary~CPU Memory
-#dictionary <int,int/Byte> ~ <address,value>
-
-#create following class variables:
-#code_start ~ text segment
-#data_start ~ data segment
-#data_stop 
-#stack_start ~ stack segment
-#heap_start ~ heap segment
-
-#create following methods/functions
-#load_data,load_word,load_byte etc.
-#store_data,store_word,store_byte etc.
-#get_next_instruction
-#get_data_at
-#add_data_at
-#show_memory
+class mem:
+  #create dictionary~CPU Memory
+  #dictionary <int,int/Byte> ~ <address,value>
+  memory = {}
+ 
+  #create following class variables:
+  #code_start ~ text segment
+  #data_start ~ data segment
+  #data_stop 
+  #stack_start ~ stack segment
+  #heap_start ~ heap segment
+  code_start=0
+  data_start=0
+  datat_stop=0
+  stack_start=0
+  heap_start=0
+  
+  #create following methods/functions
+  #load_data,load_word,load_byte etc.
+  def load_data():
+  def load_word():
+  def load_byte():
+    
+  #store_data,store_word,store_byte etc.
+  def store_data():
+  def store_word():
+  def store_byte():
+    
+  #get_next_instruction
+  def next_instruction():
+  #get_data_at
+  def get_data():
+  #add_data_at
+  def add_data():
+  #show_memory
+  def show_mem():
+  
 
 #create PC class
-#create following methods
-#add_PC
-#get_PC
-
+class PC:  
+  #create following methods
+  #add_PC
+  #get_PC
+  def add_PC():
+  def get_PC():
 
 
 #create Registers class:
-#create 32 registers
-#create following methods
-#load_from_register
-#store_to_register
-#print_register_value
-
+class reg:  
+  #create 32 registers
+  regs=[[0 for i in range(32)] for i in range(32)] 
+  
+  #create following methods
+  #load_from_register
+  #store_to_register
+  #print_register_value
+  def load():
+  def store():
+  def print():
+  
+  
 #create ... 
 
